83da09566d090a7a471aab1f6a19ebe05428c524
frogface156 <max.benson156@gmail.com>
2019-01-20 11:48:37 +0000

    solution to IMU problem

--- sensor_tests/IMU_test_1.py
+++ sensor_tests/IMU_test_1.py
@@ -1,8 +1,12 @@
+import time
 import busio
 import board
 import adafruit_bno055
 
 i2c = busio.I2C(board.SCL, board.SDA)
+time.sleep(1) # required or else smbus fails and you spend years debugging code and googling
 sensor = adafruit_bno055.BNO055(i2c)
 
-print(sensor.temperature)
+while True:
+	print(sensor.linear_acceleration)
+	time.sleep(1)
