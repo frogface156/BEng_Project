2544c48538d8cdbe7fb2669851ecde937b7169dc
frogface156 <max.benson156@gmail.com>
2019-01-20 13:02:06 +0000

    IMU probability density test

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 import imu_sensor
 
-print("Initializing sensors...")
-
 IMU = imu_sensor.IMU()
-
-print("Initialization complete!\n")
-
 while True:
-	print("Linear Acceleration: {}".format(IMU.linear_acceleration))
+	print("Linear Acceleration: {}".format(IMU.linear_acceleration[0]))
